f9a158266ebddac2bd3c52a7bb99146240ef5fb5
Harri Klingsten <powercommand2022@gmail.com>
2023-01-07 20:51:34 +0100

    refactoring

---
+++ src/Tutorials.BasicTutorial/BasicTutorial/PainKiller.PowerCommands.BasicTutorialCommands/Configuration/PowerCommandsConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace PainKiller.PowerCommands.BasicTutorialCommands.Configuration
+{
+    public class PowerCommandsConfiguration : CommandsConfiguration
+    {
+        //Here is the placeholder for your custom configuration, you need to add the change to the PowerCommandsConfiguration.yaml file as well
+        public string DefaultGitRepositoryPath { get; set; } = "C:\\repos";
+    }
+}
