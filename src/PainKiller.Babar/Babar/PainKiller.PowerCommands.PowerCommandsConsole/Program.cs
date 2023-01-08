1cf8592dae0e303f4403b9a807f5ab5737414f8d
Harri Klingsten <powercommand2022@gmail.com>
2023-01-08 22:22:23 +0100

    refactoring

--- src/PainKiller.Babar/Babar/PainKiller.PowerCommands.PowerCommandsConsole/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿using PainKiller.PowerCommands.Core.Services;
-
-ConsoleService.Service.WriteHeaderLine(nameof(Program), "Power Commands 1.0");
-PainKiller.PowerCommands.Bootstrap.Startup.ConfigureServices().Run(args);
